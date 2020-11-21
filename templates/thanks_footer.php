9e7c7687d52aeddc3ab3575939cdf7ab354deb3d
kchang00 <k_chang@fanshaweonline.ca>
2020-11-20 20:08:33 -0500

    updated resume and db

--- templates/thanks_footer.php
+++ templates/thanks_footer.php
@@ -1,5 +1,5 @@
 <section class="thanks-footer">
     <h2>Thanks for visiting!</h2>
     <img src="/public/images/footer_plant.svg" alt="Thanks">
-    <a tabindex="0" href="/public/images/resume_cv_portfolio.pdf" download class="btn-large">Download my resume</a>
+    <a tabindex="0" href="/public/images/kayla_chang_resume_cv.pdf" download class="btn-large">Download my resume</a>
 </section>
